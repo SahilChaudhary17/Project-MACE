74f10714fe42eaf82a617857dbc5be122f12fb4d
SahilChaudhary17 <sahilch38@gmail.com>
2022-09-26 19:32:00 +0530

    first commit

---
+++ Mace/lib/Doctor/ChatUI.dart
@@ -0,0 +1,54 @@
+/*import 'package:flutter/material.dart';
+
+class ChatUIScreen extends StatefulWidget {
+  const ChatUIScreen({Key? key, this.animationController}) : super(key: key);
+
+  final AnimationController? animationController;
+  @override
+  _ChatUIScreenState createState() => _ChatUIScreenState();
+}
+
+class _ChatUIScreenState extends State<ChatUIScreen>
+    with TickerProviderStateMixin {
+  Animation<double>? topBarAnimation;
+
+  List<Widget> listViews = <Widget>[];
+  final ScrollController scrollController = ScrollController();
+  double topBarOpacity = 0.0;
+
+  @override
+  void initState() {
+    topBarAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+            parent: widget.animationController!,
+            curve: Interval(0, 0.5, curve: Curves.fastOutSlowIn)));
+    addAllListData();
+
+    scrollController.addListener(() {
+      if (scrollController.offset >= 24) {
+        if (topBarOpacity != 1.0) {
+          setState(() {
+            topBarOpacity = 1.0;
+          });
+        }
+      } else if (scrollController.offset <= 24 &&
+          scrollController.offset >= 0) {
+        if (topBarOpacity != scrollController.offset / 24) {
+          setState(() {
+            topBarOpacity = scrollController.offset / 24;
+          });
+        }
+      } else if (scrollController.offset <= 0) {
+        if (topBarOpacity != 0.0) {
+          setState(() {
+            topBarOpacity = 0.0;
+          });
+        }
+      }
+    });
+    super.initState();
+  }
+  void addAllListData() {
+    const int count = 9;
+  }
+}*/
